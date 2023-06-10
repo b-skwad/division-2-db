2e9257b01c83c8b118b2f4438097121078f99d56
The Fail Druid <faildruid@mcleans.ie>
2023-06-11 00:35:03 +0100

    Added Models and Unit Tests for
    - Loadout Slots
    - Gear Attributes & Attribute Types
    - Gear Modifications & Modification Types
    - Gear Talents & Talent Types

--- division/core/tests/test_models.py
+++ division/core/tests/test_models.py
@@ -3,7 +3,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-# from division.core import models
+from division.core import models
 
 
 class ModelTests(TestCase):
@@ -47,3 +47,100 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_gear_talent_type(self):
+        """Test creating a gear set."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        gear_talent_type = models.GearTalentType.objects.create(user=user, talent_type_name="Weapon Talent")
+
+        self.assertEquals(str(gear_talent_type), gear_talent_type.talent_type_name)
+
+    def test_create_gear_talent(self):
+        """Test creating a gear set."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        gear_talent_type = models.GearTalentType.objects.create(user=user, talent_type_name="Weapon Talent")
+        gear_talent = models.GearTalent.objects.create(
+            user=user,
+            talent_type=gear_talent_type,
+            talent_name="Perfect Test Talent",
+            talent_description="Perfect Test Talent Description",
+        )
+        self.assertEquals(str(gear_talent), gear_talent.talent_name)
+
+    def test_create_load_out_slot(self):
+        """Test creating a load-out slot."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        load_out_slot = models.LoadOutSlot.objects.create(user=user, load_out_slot_name="Primary Weapon")
+
+        self.assertEquals(str(load_out_slot), load_out_slot.load_out_slot_name)
+
+    def test_create_gear_attribute_type(self):
+        """Test creating a gear attribute type."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        gear_attribute_type = models.GearAttributeType.objects.create(
+            user=user, gear_attribute_type_name="Weapon attribute"
+        )
+
+        self.assertEquals(str(gear_attribute_type), gear_attribute_type.gear_attribute_type_name)
+
+    def test_create_gear_modification_type(self):
+        """Test creating a gear mod type."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        gear_modification_type = models.GearModificationType.objects.create(
+            user=user, gear_modification_type_name="Weapon Modification"
+        )
+
+        self.assertEquals(str(gear_modification_type), gear_modification_type.gear_modification_type_name)
+
+    def test_create_gear_modification(self):
+        """Test creating a gear mod."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        gear_modification_type = models.GearModificationType.objects.create(
+            user=user, gear_modification_type_name="Weapon Modification"
+        )
+
+        gear_modification = models.GearModification.objects.create(
+            user=user,
+            gear_modification_type=gear_modification_type,
+            gear_modification_name="Weapon attribute",
+            max_value=750,  # Representing 7.5% as an integer, will divide by 100 for any display,
+            percent_value=True,
+        )
+
+        self.assertEquals(str(gear_modification), gear_modification.gear_modification_name)
+
+    def test_create_gear_attribute(self):
+        """Test creating a gear attribute."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        gear_attribute_type = models.GearAttributeType.objects.create(user=user, gear_attribute_type_name="Weapon Attr")
+
+        gear_attribute = models.GearAttribute.objects.create(
+            user=user,
+            gear_attribute_type=gear_attribute_type,
+            gear_attribute_name="Weapon attribute",
+            max_value=750,  # Representing 7.5% as an integer, will divide by 100 for any display,
+            percent_value=True,
+        )
+
+        self.assertEquals(str(gear_attribute), gear_attribute.gear_attribute_name)
